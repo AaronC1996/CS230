7321b18677334304a6975b0dc68f8df3a2e2cfba
AaronC1996 <48731100+AaronC1996@users.noreply.github.com>
2019-03-19 18:54:01 +0000

    Add files via upload

---
+++ eBook_MetaData(1).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 19, 2019 at 07:46 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Assignment3_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eBook_MetaData`
+--
+
+CREATE TABLE `eBook_MetaData` (
+  `ID` bigint(20) UNSIGNED NOT NULL,
+  `Creator` text NOT NULL,
+  `Title` text NOT NULL,
+  `Type` text NOT NULL,
+  `Identifier` text NOT NULL,
+  `Date` date NOT NULL,
+  `Language` text NOT NULL,
+  `Description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `eBook_MetaData`
+--
+
+INSERT INTO `eBook_MetaData` (`ID`, `Creator`, `Title`, `Type`, `Identifier`, `Date`, `Language`, `Description`) VALUES
+(1, 'Aaron Cooney', 'CS230', 'Sci-Fi', 'ISBN', '2019-03-19', 'en-GB', 'Web Information Processing'),
+(2, 'Owen Dunne', 'CS253', 'Non-Fiction', 'ISBN', '2019-03-14', 'fr-FR', 'Computer Architecture'),
+(3, 'Lucy Dowling', 'CS335', 'Young Adult', 'URI', '2019-01-11', 'de-DE', 'Software Engineering & Processing'),
+(5, 'Mark Coady', 'CS265', 'Non-Fiction', 'URN', '2019-02-06', 'es-ES', 'Software Testing'),
+(7, 'Brian Dunne', 'CS267', 'Non-Fiction', 'DUI', '2018-08-08', 'de-DE', 'Object-Orientated Programming');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `eBook_MetaData`
+--
+ALTER TABLE `eBook_MetaData`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `ID` (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `eBook_MetaData`
+--
+ALTER TABLE `eBook_MetaData`
+  MODIFY `ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
